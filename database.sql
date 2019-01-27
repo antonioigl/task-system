6907bc17b2e8a7be43c09a47f4b85fcc04456892
Antonio <antonio241710@gmail.com>
2019-01-27 17:24:17 +0100

    459-Rellenar la base de datos

--- database.sql
+++ database.sql
@@ -12,6 +12,9 @@ CREATE TABLE IF NOT EXISTS users(
   CONSTRAINT              pk_users PRIMARY KEY (id)
   ) ENGINE=InnoDb;
 
+INSERT INTO users VALUES (NULL, 'ROLE_USER', 'Victor', 'Robles', 'victor@victor.com', 'password', CURTIME());
+INSERT INTO users VALUES (NULL, 'ROLE_USER', 'Manolo', 'Perez', 'manolo@manolo.com', 'password', CURTIME());
+INSERT INTO users VALUES (NULL, 'ROLE_USER', 'Carlos', 'Sanchez', 'carlos@carlos.com', 'password', CURTIME());
 
 CREATE TABLE IF NOT EXISTS tasks(
   id                      int(255) auto_increment not null,
@@ -24,3 +27,8 @@ CREATE TABLE IF NOT EXISTS tasks(
   CONSTRAINT              pk_tasks PRIMARY KEY (id),
   CONSTRAINT              fk_task_user FOREIGN KEY (user_id) REFERENCES users(id)
   ) ENGINE=InnoDb;
+
+INSERT INTO tasks VALUES (NULL, 1, 'Tarea 1', 'Contenido de prueba 1', 'high', 40, CURTIME());
+INSERT INTO tasks VALUES (NULL, 1, 'Tarea 2', 'Contenido de prueba 2', 'low', 20, CURTIME());
+INSERT INTO tasks VALUES (NULL, 2, 'Tarea 3', 'Contenido de prueba 3', 'medium', 10, CURTIME());
+INSERT INTO tasks VALUES (NULL, 3, 'Tarea 4', 'Contenido de prueba 4', 'high', 50, CURTIME());
