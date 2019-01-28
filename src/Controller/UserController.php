a1c37fb5e3c5eb2a0e3e73795e7e185c91d727f7
Antonio <antonio241710@gmail.com>
2019-01-28 22:41:56 +0100

    461-Formulario de registro

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,16 +4,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
+use App\Form\RegisterType;
 
 class UserController extends AbstractController
 {
-    /**
-     * @Route("/user", name="user")
-     */
-    public function index()
+
+    public function register(Request $request)
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        return $this->render('user/register.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
